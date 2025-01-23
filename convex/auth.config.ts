91a11b26b9323ecafa45dde9078ee7cfc3b9e91e
Vicky <vikramventures2024@gmail.com>
2025-01-23 19:38:10 +0530

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://enabling-alien-47.clerk.accounts.dev/",
+      domain: "https://pleasing-hare-94.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
